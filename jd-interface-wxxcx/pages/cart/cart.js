6209e6ab1e89e8383811d35bdbb28314d9c2ac76
fya0122 <18730930122@163.com>
2019-03-03 12:06:20 +0800

    'cart_loading'

--- jd-interface-wxxcx/pages/cart/cart.js
+++ jd-interface-wxxcx/pages/cart/cart.js
@@ -2,8 +2,12 @@ Page({
   data: {
     cartList: []
   },
-  onLoad: function(options) {
+  onShow() {
     this._getStorage()
+  },
+  onHide() {
+    wx.removeStorageSync('cartListInfo')
+    wx.setStorageSync('cartListInfo', JSON.stringify(this.data.cartList))
   },
   _getStorage() {
     let cartListInfo = wx.getStorageSync('cartListInfo')
@@ -12,6 +16,7 @@ Page({
       this.setData({
         cartList: cartListInfo
       }, () => {
+        console.log(this.data.cartList)
         if (this.data.cartList.length > 0) {
           // 设置购物车图标
           wx.setTabBarBadge({
@@ -25,5 +30,26 @@ Page({
         }
       })
     }
+  },
+  // 改变你点击的那个购物车的数量呢
+  getcount(e) {
+    const id = e.currentTarget.dataset.id
+    const inputvalue = e.detail.inputvalue
+    let cartList = this.data.cartList
+    for (const item of cartList) {
+      if (item.id === id) {
+        item.buyTotal = inputvalue
+      }
+    }
+    this.setData({
+      cartList: cartList
+    })
+  },
+  // 跳转到指定detail的吗？
+  switchGoodsDetail(e) {
+    const id = e.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '../listdetail/listdetail?id=' + id
+    })
   }
 })
