795a3744e748dac756e563296585ac84a11d4aff
fya0122 <18730930122@163.com>
2019-03-04 21:01:40 +0800

    'end'

--- jd-interface-wxxcx/pages/cart/cart.js
+++ jd-interface-wxxcx/pages/cart/cart.js
@@ -3,7 +3,9 @@ Page({
     cartList: [],
     totalMoney: 0, // 总价格
     totalCount: 0, // 总数量
-    selectAll: false // 是否全选
+    selectAll: false, // 是否全选
+    startX: 0, // 开始坐标
+    startY: 0 // 开始纵坐标
   },
   onShow() {
     this._getStorage()
@@ -18,7 +20,7 @@ Page({
     if (cartListInfo) {
       cartListInfo = JSON.parse(cartListInfo)
       cartListInfo.forEach((item) => {
-        item.isTouchMove = true // 是否滑动，肯定是false的，当为true的时候，显示另一个样式呢
+        item.isTouchMove = false // 是否滑动，肯定是false的，当为true的时候，显示另一个样式呢
       })
       this.setData({
         cartList: cartListInfo
@@ -147,6 +149,80 @@ Page({
         item.select = true
       })
     }
+    this.setData({
+      cartList
+    }, () => {
+      this._setBadgeCount(this.data.cartList)
+      this._calculateTotalPriceAndCount(this.data.cartList)
+      this._calculateIsAllSelect(this.data.cartList)
+    })
+  },
+  // 左滑开始
+  delbindtouchstart(e) {
+    let cartList = this.data.cartList
+    cartList.forEach((item) => {
+      item.isTouchMove = false
+    })
+    this.setData({
+      startX: e.changedTouches[0].clientX,
+      startY: e.changedTouches[0].clientY,
+      cartList
+    })
+  },
+  // 左滑中
+  delbindtouchmove(e) {
+    const id = e.currentTarget.dataset.id
+
+    // 开始x和y坐标
+    let startX = this.data.startX
+    let startY = this.data.startY
+
+    // 移动的
+    let touchMoveX = e.changedTouches[0].clientX
+    let touchMoveY = e.changedTouches[0].clientY
+
+    // 开始计算
+    const result = this._angel({
+      x: startX,
+      y: startY
+    }, {
+      x: touchMoveX,
+      y: touchMoveY
+    })
+
+
+    let cartList = this.data.cartList
+    cartList.forEach((item) => {
+      item.isTouchMove = false
+      if (Math.abs(result) > 30) {
+        return
+      }
+      if (item.id === id) {
+        // 右滑动
+        if (touchMoveX > startX) {
+          item.isTouchMove = false
+        } else {
+          item.isTouchMove = true
+        }
+      }
+    })
+    this.setData({
+      cartList
+    })
+  },
+  // 算角度
+  _angel(start, end) {
+    let _x = end.x - start.x
+    let _y = end.y - start.y
+
+    // 返回角度Math.atan() 返回数据的反正切值
+    return 360 * Math.atan(_y / _x) / (2 * Math.PI)
+  },
+  // 删除某个商品呢
+  delItem(e) {
+    const index = e.currentTarget.dataset.index
+    let cartList = this.data.cartList
+    cartList.splice(index, 1)
     this.setData({
       cartList
     }, () => {
